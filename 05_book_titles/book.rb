7021d1fdbf5461930b94380f56c8184b94896ad1
Bartosz Zagozda <bzagozda@hotmail.com>
2016-07-26 20:45:26 +0200

    done 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
-class Book
 # write your code here
+class Book
+  attr_reader :title
+
+  def title=(book_title)
+    not_to_capitalize = %w(and in the of a an)
+    words = book_title.split
+    words = [words[0].capitalize] + words[1..-1].map do |word|
+      (not_to_capitalize.include?(word) ? word : word.capitalize)
+    end
+    @title=words.join(" ")
+  end
 end
