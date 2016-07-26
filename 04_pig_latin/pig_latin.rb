8e425eb190375103e056cb0cb395e79bd3d5a550
Bartosz Zagozda <bzagozda@hotmail.com>
2016-07-26 15:24:44 +0200

    done 0_4_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate text
+	text.split.map! do |word|
+		i=0
+		while !vovel?(word[i,1])
+			if word[i,2] == 'qu'
+				i+=2
+			else
+				i+=1
+			end
+		end
+		word[i..-1] + word[0,i] + "ay"
+	end.join(" ")
+end
+
+def vovel? word
+	"euyioa".include?(word)
+end	
