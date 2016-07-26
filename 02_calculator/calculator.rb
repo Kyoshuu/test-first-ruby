91a59a5cfce504640d7a19da10486746a0fb77e2
Bartosz Zagozda <bzagozda@hotmail.com>
2016-07-26 12:42:22 +0200

    done 01 and 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add(n1, n2)
+	n1 + n2
+end
+
+def subtract(n1, n2)
+	n1 - n2
+end
+
+def sum(array)
+	sum = 0
+	array.each do |n|
+		sum += n
+	end
+	sum
+end
+
+def multiply(*nums)
+	mul = 1
+	nums.each do |n|
+		mul *= n
+	end
+	mul
+end
+
+def power(n1, n2)
+	pow = 1
+	n2.times do
+		pow *= n1
+	end
+	pow
+end
+
+def factorial(num)
+	return (num == 0 ? 1 : num * factorial(num - 1))
+end
